bbdec7b23b64db9e03d52a223b089f77d8d1c758
anoff <offenhaeuser@gmail.com>
2017-06-23 00:16:24 +0200

    :star2: feat: increase readability

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const backgroundColor = '#FFFFFF'
 const foregroundColor = '#272822'
 
-const cursorColor = '#f0f8ff'
+const cursorColor = 'rgba(249,38,114,0.8)'
 
 const colors = {
   black: '#272822',
   red: '#F92672',
   green: '#A6E22E',
-  yellow: '#FD971F',
+  yellow: '#F25A00',
   blue: '#AE81FF',
   magenta: '#F92672',
   cyan: '#28C6E4',
@@ -15,7 +15,7 @@ const colors = {
   lightBlack: '#49483e',
   lightRed: '#F92672',
   lightGreen: '#A6E22E',
-  lightYellow: '#FD971F',
+  lightYellow: '#F25A00',
   lightBlue: '#AE81FF',
   lightMagenta: '#F92672',
   lightCyan: '#28C6E4',
@@ -27,14 +27,7 @@ exports.decorateConfig = config => {
   return Object.assign({}, config, {
     foregroundColor,
     backgroundColor,
-    borderColor,
     cursorColor,
-    colors,
-    css: `
-      ${config.css || ''}
-      .tab_active:before {
-        border-bottom-color: ${backgroundColor};
-      }
-    `
+    colors
   })
 }
