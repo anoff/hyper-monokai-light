7e93c80da396cbf2b9787971584578e7068f6b55
anoff <offenhaeuser@gmail.com>
2017-06-25 21:47:27 +0200

    :star2: feat: darkened blue/green

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ const cursorColor = 'rgba(249,38,114,0.8)'
 const colors = {
   black: '#272822',
   red: '#F92672',
-  green: '#A6E22E',
+  green: '#6AAF19',
   yellow: '#F25A00',
   blue: '#AE81FF',
   magenta: '#F92672',
-  cyan: '#28C6E4',
+  cyan: '##22B1CC',
   white: '#f8f8f0',
   lightBlack: '#49483e',
   lightRed: '#F92672',
-  lightGreen: '#A6E22E',
+  lightGreen: '#6AAF19',
   lightYellow: '#F25A00',
   lightBlue: '#AE81FF',
   lightMagenta: '#F92672',
-  lightCyan: '#28C6E4',
+  lightCyan: '##22B1CC',
   lightWhite: '#f8f8f2'
 }
 
